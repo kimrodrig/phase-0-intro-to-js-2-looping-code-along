67cac9eba2aa0450a163a870f8b977be988f0db3
Kim <rodrigkim@gmail.com>
2022-08-30 09:48:23 -0400

    commit tree 3cf706de36c1d4d95e3c88b980a6445379a0fa1b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array, event){
+    const thankYous = [];
+    for (name in array){
+        thankYous.push(`Thank you, ${array[name]}, for the wonderful ${event} gift!`)
+    }
+    return thankYous;
+}
+
+function countDown(x){
+    while (x >= 0){
+        console.log(x);
+        x --;
+    }
+}
